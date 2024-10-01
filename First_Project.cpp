9d3c542984ef5f33eff18d4e8ef0da48ca3546d6
Khushil-Vekaria <khushil.vekaria@strathmore.edu>
2024-10-01 19:55:44 +0300

    Dbit

---
+++ First_Project.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    cout << "This is my forst project in introduction to programming";
+    cout << "i am doing DBIT";
+    return 0;
+
+}
