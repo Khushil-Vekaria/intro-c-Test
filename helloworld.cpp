e1517cf74f4d6958d3743b63160a211dd120c395
Khushil-Vekaria <khushil.vekaria@strathmore.edu>
2024-10-01 19:52:38 +0300

    first project

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
 
-    cout << "Helloe World . Glad to begin learning c++";
+    cout << "Helloe World 123456789. Glad to begin learning c++";
     cout << "My name is Khushil Vekaria";
     return 0;
 
