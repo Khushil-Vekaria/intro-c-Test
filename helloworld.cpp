7808e28dce8d79473148aadb306e279853824a87
Khushil-Vekaria <khushil.vekaria@strathmore.edu>
2024-10-11 11:02:27 +0300

    8/10/24

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,6 +5,7 @@ int main(){
 
     cout << "Helloe World 123456789. Glad to begin learning c++";
     cout << "My name is Khushil Vekaria";
+    cout << "the first lines of code";
     return 0;
 
 }
