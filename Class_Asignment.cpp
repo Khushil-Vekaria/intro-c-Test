40b609c53e93da2f1db93d687f41a0367313171a
Khushil-Vekaria <khushil.vekaria@strathmore.edu>
2024-10-15 20:21:56 +0300

    15/10/2024

---
+++ Class_Asignment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string part1 = "Strathmore University is the leading ";
+    string part2 = "university in the region";
+    string sentence = part1 + part2;
+    cout << sentence << std::endl;
+    return 0;
+}
---
+++ escapesequence1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+      cout << "BBIT is a university diploma designed to focus on :\n";
+
+    cout << "\t1. Development\n";
+
+     cout << "\t2. Implementation, and\n";
+
+      cout << "\t3. Management of information systems\n";
+
+       return 0;
+
+}
