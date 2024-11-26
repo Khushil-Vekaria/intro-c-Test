2641809dbf4ad7a9e0a2a397192dbc9677338faa
Khushil-Vekaria <khushil.vekaria@strathmore.edu>
2024-11-26 20:43:36 +0300

    26/11/2024

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double nums [] = {12.25, 32.50, 16.90, 23, 45.68, 50.60};
+    for(int i = 0; i < 6; i++) {
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
